968779784b295ff42784ad8a63b91e6c4ad3e8ce
NEEEMO-LIO <liuedwardnemo@gmail.com>
2025-11-01 19:56:39 -0700

    update4

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* POST form handler */
+router.post('/', function(req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  res.send(`Thank you, ${name}! We received your email: ${email}`);
 });
 
 module.exports = router;
